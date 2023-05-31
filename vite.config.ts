686b1f77ae03d42a387a92347abef5a0b2a70325
Lukas Stracke <lukas.stracke@sentry.io>
2023-05-31 12:03:26 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sentrySvelteKit(), sveltekit()]
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    sentrySvelteKit({
+      autoUploadSourceMaps: true,
+      sourceMapsUploadOptions: {
+        include: [
+          { paths: [`.svelte-kit/output/client`] },
+          { paths: [`.svelte-kit/output/server/chunks`] },
+          { paths: [`.svelte-kit/output/server`], ignore: ["chunks/**"] },
+        ],
+      },
+    }),
+    sveltekit(),
+  ],
 });
